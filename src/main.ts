e6a5857cf9108816c64477f2f715d1cd06878194
fawaz <fawazodessy@gmail.com>
2023-04-29 09:55:56 +0100

    Multi Speeches

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,39 @@
 import './style.css'
-
 const textarea = document.querySelector('textarea') as HTMLTextAreaElement
 const button = document.querySelector('button') as HTMLButtonElement
+const select = document.querySelector('select') as HTMLSelectElement
+
 let isSpeaking = true
+const synth = speechSynthesis
+
+const populateVoices = () => {
+  // mapping through the list of voices
+  for(const voice of synth.getVoices()){
+    // setting US English as default
+    const selected = voice.name === 'Google US English' ? 'selected' : ''
+    // creating option tags of voices 
+    const option = `<option value="${voice.name}" ${selected}>${voice.name}</option>`
+    select.insertAdjacentHTML('beforeend' , option)
+  }
+}
+populateVoices()
+
+synth.addEventListener('voiceschanged' , populateVoices)
 
 const textToSpeech = () => {
-  const synth = window.speechSynthesis
   const text = textarea.value
 
   if (!synth.speaking && text) {
     const speech = new SpeechSynthesisUtterance(text)
+    const selectedVoice = select.value // Get the selected voice
+    const voices = synth.getVoices()
+    const voice = voices.find((v) => v.name === selectedVoice)
+    if (voice) {
+      speech.voice = voice // Set the voice property of the SpeechSynthesisUtterance object to the selected voice
+    }
     synth.speak(speech)
   }
+  
 
   if (text.length > 50) {
     if (synth.speaking && isSpeaking) {
@@ -23,6 +45,9 @@ const textToSpeech = () => {
       synth.pause()
       isSpeaking = true
     }
+  } else {
+    isSpeaking = false
+    button.innerText = 'Speaking'
   }
 
   setInterval(() => {
@@ -34,3 +59,17 @@ const textToSpeech = () => {
 }
 
 button.addEventListener('click' , textToSpeech)
+
+select.addEventListener('change', () => {
+  // const text = textarea.value
+  if (synth.speaking) {
+    synth.cancel() // Stop the current speech
+    isSpeaking = false
+    button.innerText = 'Convert to Speech'
+    setTimeout(() => {
+      textToSpeech() // Start speech with the newly selected voice
+    }, 500)
+  } else {
+    textToSpeech() // Start speech with the newly selected voice
+  }
+})
